fcd4a242dc6faa8a61c1845334446e43b43baa6f
Vlad <vlad.sad28@yandex.ru>
2026-03-30 15:41:08 +0300

    wip: save progress: header

---
+++ src/components/general/header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.scss';
+
+
+
+const Header: React.FC = () => {
+    return (
+        <header className="header container">
+            Header
+        </header>
+    )
+}
+
+export default Header;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 
 import './index.scss'
 
-
+import Header from '../../components/general/header';
 
 
 const Main: React.FC = () => {
     return (
         <>
-            <h1 className="heading-128-magra-bold whiteText">FRONTEND DEVELOPER</h1>
-            <div className="noize testRectangle radius-full shadow-inner"></div>
-            <div className="noize testRectangle radius-full shadow-2xl"></div>  
-            <div className="testRectangle zzz radius-full glass"></div>  
-            <div className="overlay glass"></div>
+            <Header />
         </>
        
     )
