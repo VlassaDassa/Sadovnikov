7f10aebe59ec7992abe43e0c78dc991ad3ab7aa8
Vlad <vlad.sad28@yandex.ru>
2026-03-30 11:37:32 +0300

    style: Typography classes and effects have been added

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,9 +4,17 @@ import './index.scss'
 
 
 
+
 const Main: React.FC = () => {
     return (
-        <h1 className="test">Hello world!</h1>
+        <>
+            <h1 className={'heading-128-magra-bold whiteText'}>FRONTEND DEVELOPER</h1>
+            <div className="noize testRectangle radius-full shadow-inner"></div>
+            <div className="noize testRectangle radius-full shadow-2xl"></div>  
+            <div className="testRectangle zzz radius-full glass"></div>  
+            <div className="overlay glass"></div>
+        </>
+       
     )
 }
 
