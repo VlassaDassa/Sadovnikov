5264d29bb6a6f9f420f8e62ae2f48f66e9bd0fde
Vlad <vlad.sad28@yandex.ru>
2026-03-30 11:50:17 +0300

    fix: missing typography classes have been added and README has been fixed

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -8,7 +8,7 @@ import './index.scss'
 const Main: React.FC = () => {
     return (
         <>
-            <h1 className={'heading-128-magra-bold whiteText'}>FRONTEND DEVELOPER</h1>
+            <h1 className="heading-128-magra-bold whiteText">FRONTEND DEVELOPER</h1>
             <div className="noize testRectangle radius-full shadow-inner"></div>
             <div className="noize testRectangle radius-full shadow-2xl"></div>  
             <div className="testRectangle zzz radius-full glass"></div>  
