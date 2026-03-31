ef4c9beb2a215e04e9e3723c65155825a671e92b
vlad <vlad.sad28@yandex.ru>
2026-03-31 20:47:44 +0300

    feat: added a header

--- src/components/general/header/index.tsx
+++ src/components/general/header/index.tsx
@@ -7,7 +7,21 @@ import './index.scss';
 const Header: React.FC = () => {
     return (
         <header className="header container">
-            Header
+            <a className="btn logo" href="#" aria-label="Логотип">
+                <p className="btnText body-16-magra-bold whiteText">
+                    SADOVNIKOV
+                </p>
+            </a>
+            
+
+            <button 
+                className="btnBurger radius-full"
+                aria-label="Открыть меню"
+            >
+                <span className="radius-12"></span>
+                <span className="radius-12"></span>
+                <span className="radius-12"></span>
+            </button>
         </header>
     )
 }
