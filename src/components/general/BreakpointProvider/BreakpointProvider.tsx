719974b9aa38f86a3a1672ec1d4329d12a91a367
Vlad <vlad.sad28@yandex.ru>
2026-04-23 11:59:45 +0300

    feat: добавлена задержка в BreakpointProvider при перерасчёте размеров. Ресайз теперь оптимизирован

--- src/components/general/BreakpointProvider/BreakpointProvider.tsx
+++ src/components/general/BreakpointProvider/BreakpointProvider.tsx
@@ -5,8 +5,6 @@ import { useDispatch } from 'react-redux'
 import { setBreakpoint } from "@/store/slices/breakpointSlice"
 import type { Breakpoint } from "@/interfaces/general"
 
-
-
 interface BreakpointProviderProps {
     children: React.ReactNode;
 }
@@ -15,26 +13,36 @@ export const BreakpointProvider = ({ children }: BreakpointProviderProps ) => {
     const dispatch = useDispatch()
 
     useEffect(() => {
+        let timeout: NodeJS.Timeout;
+
         const updateBreakpoint = () => {
             const width = window.innerWidth
             let breakpoint: Breakpoint
 
             if (width >= 1280) {
-                breakpoint='desktop'
-            }
-            else if (width >= 768) {
-                breakpoint='tablet'
-            }
-            else {
-                breakpoint='mobile'
+                breakpoint = 'desktop'
+            } else if (width >= 768) {
+                breakpoint = 'tablet'
+            } else {
+                breakpoint = 'mobile'
             }
 
             dispatch(setBreakpoint(breakpoint))
         }
-        
+
+        const debouncedUpdate = () => {
+            clearTimeout(timeout)
+            timeout = setTimeout(updateBreakpoint, 150)
+        }
+
+        // Запускаем сразу при монтировании
         updateBreakpoint()
-        window.addEventListener('resize', updateBreakpoint)
-        return () => window.removeEventListener('resize', updateBreakpoint)
+
+        window.addEventListener('resize', debouncedUpdate)
+        return () => {
+            window.removeEventListener('resize', debouncedUpdate)
+            clearTimeout(timeout)
+        }
     }, [dispatch])
 
     return <>{children}</>
