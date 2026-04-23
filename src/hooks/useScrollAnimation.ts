8e4671e5edfeaeccd71c33ad05e9861bf959423b
Vlad <vlad.sad28@yandex.ru>
2026-04-23 17:44:50 +0300

    wip: создание общих анимаций

---
+++ src/hooks/useScrollAnimation.ts
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+
+interface UseScrollAnimationProps {
+    threshold?: number,
+    rootMargin?: string,
+    animationClass?: string,
+}
+
+export const useScrollAnimation = <T extends HTMLElement>({
+    threshold = 0.1,
+    rootMargin = '0px 0px -50px 0px',
+    animationClass = 'animate-in'
+}: UseScrollAnimationProps = {}) => {
+
+    const elementRef = useRef<T>(null);
+    const [isVisible, setIsVisible] = useState(false)
+
+    useEffect(() => {
+        const currentElement = elementRef.current;
+
+        if (!currentElement) return;
+
+        const observer = new IntersectionObserver
+    }, [])
+
+}
