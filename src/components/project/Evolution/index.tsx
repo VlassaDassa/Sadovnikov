add7ba9914e23917aad6cc5d986834d39b8b26ad
Vlad <vlad.sad28@yandex.ru>
2026-05-07 02:13:43 +0300

    feat: отображение tooltip при hover на commit

--- src/components/project/Evolution/index.tsx
+++ src/components/project/Evolution/index.tsx
@@ -1,9 +1,14 @@
+'use client'
+
 import React from 'react';
+
+import { useTooltip } from '@/hooks/useTooltip';
 
 import styles from './index.module.scss';
 
 
 interface ICommit {
+    id: number,
     name: string,
     date: string,
     text: string
@@ -12,27 +17,54 @@ interface ICommit {
 
 const commits: ICommit[] = [
     {
+        id: 1,
         name: 'First commit',
         date: '22.04.2026',
         text: 'First initialization commit, start development project'
     },
     {
+        id: 2,
         name: 'Added “Header”',
         date: '22.04.2026',
         text: 'First initialization commit, start development project'
     },
     {
-        name: '- Fix “Preview”',
+        id: 3,
+        name: 'Fix “Preview”',
         date: '22.04.2026',
         text: 'First initialization commit, start development project'
     },
     {
+        id: 4,
         name: 'Added “Contacts”, fix errors, animations',
         date: '22.04.2026',
         text: 'First initialization commit, start development project'
     },
 ]
 
+interface CommitProps {
+    item: ICommit
+}
+
+
+const Commit: React.FC<CommitProps> = ({ item }) => {
+    const tooltipRef = useTooltip<HTMLDivElement>({
+            text: item.text,
+            title: item.name,
+            date: item.date,
+            type: 'lvl3',
+            placement: 'bottom',
+            fakeWidth: 400,
+            delay: 400,
+        });
+
+    return (
+        <div ref={tooltipRef} key={item.name} className={styles.commitItem}>
+            <div className={styles.node}></div>
+            <p className={styles.commitText}>- {item.name}</p>
+        </div>
+    )
+}
 
 
 const Evolution: React.FC = () => {
@@ -43,10 +75,7 @@ const Evolution: React.FC = () => {
             <div className={styles.evolWrapper}>
                 <div className={styles.timeline}>
                     {commits.map((item) => (
-                        <div key={item.name} className={styles.commitItem}>
-                            <div className={styles.node}></div>
-                            <p className={styles.commitText}>- {item.name}</p>
-                        </div>
+                        <Commit key={item.id} item={item} />
                     ))}
                 </div>
             </div>
