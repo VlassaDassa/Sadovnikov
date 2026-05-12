608883844e10a51525d920dbbd9a87efa98936fb
vlad <vlad.sad28@yandex.ru>
2026-05-12 08:13:41 +0300

    wip: адаптация под desktop

--- src/components/general/paginationSlider/index.tsx
+++ src/components/general/paginationSlider/index.tsx
@@ -5,14 +5,15 @@ import styles from './index.module.scss';
 
 interface PaginationSliderProps {
     totalCountItems: number,
-    curIndex: number
+    curIndex: number,
+    className: string,
 }
 
 
 
-const PaginationSlider: React.FC<PaginationSliderProps> = ({ totalCountItems, curIndex }) => {
+const PaginationSlider: React.FC<PaginationSliderProps> = ({ totalCountItems, curIndex, className }) => {
     return (
-        <div className={styles.sliderPgnWrapper}>
+        <div className={`${styles.sliderPgnWrapper} ${className}`}>
             {
                 Array.from({ length: totalCountItems }).map((item, index) => (
                     <div 
--- src/components/project/ProjectPreview/index.tsx
+++ src/components/project/ProjectPreview/index.tsx
@@ -1,12 +1,8 @@
-'use client'
+import React from 'react';
 
-import React, { useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-
-import PaginationSlider from '@/components/general/paginationSlider';
 import Button from '@/components/general/button/Button';
 import Icon from '@/components/icons/Icon';
+import Slider from '../Slider';
 import AdaptiveImage from '@/components/general/AdaptiveImage';
 
 import styles from './index.module.scss';
@@ -16,8 +12,8 @@ import { cssVars } from '@/styles/cssVariables';
 const projectImg1 = '/images/mockImages/specTecno.png'
 const testPhoto = '/images/mockImages/testPhoto.png'
 
+
 const ProjectPreview: React.FC = () => {
-    const [curIndex, setCurIndex] = useState<number>(1)
 
     return (
         <section className={`${styles.projectPreview} container`}>
@@ -91,52 +87,9 @@ const ProjectPreview: React.FC = () => {
                 </div>
             </div>
 
-            <div className={styles.photoContainer}>
-                <Swiper
-                    slidesPerView={1}
-                    spaceBetween={30}  
-                    onRealIndexChange={(swiper) => setCurIndex(swiper.activeIndex + 1)}
-                    pagination={true}   
-                    className={styles.swiper}   
-                >
-                    <SwiperSlide>
-                        <AdaptiveImage 
-                            src={projectImg1}
-                            alt={'project'}
-                            loading='eager'
-                            ariaHidden={false}
-                            wrapClass={styles.projectWrapPhoto}
-                            imgClass={styles.projectPhoto}
-                            ariaLabel={'photo'}
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <AdaptiveImage 
-                            src={testPhoto}
-                            alt={'project'}
-                            loading='eager'
-                            ariaHidden={false}
-                            wrapClass={styles.projectWrapPhoto}
-                            imgClass={styles.projectPhoto}
-                            ariaLabel={'photo'}
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <AdaptiveImage 
-                            src={projectImg1}
-                            alt={'project'}
-                            loading='eager'
-                            ariaHidden={false}
-                            wrapClass={styles.projectWrapPhoto}
-                            imgClass={styles.projectPhoto}
-                            ariaLabel={'photo'}
-                        />
-                    </SwiperSlide>
+            <Slider />
 
-                    <PaginationSlider totalCountItems={3} curIndex={curIndex} />
-                </Swiper>
-            </div>
-            
+            {/* <AdaptiveImage /> */}
         </section>
     )
 }
