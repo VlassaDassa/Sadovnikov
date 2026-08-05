89994f20ad3093a007856fb599db4a0d95fddb61
vlad <vlad.sad28@yandex.ru>
2026-08-05 21:51:44 +0300

    wip: попытка добавить рекламу

--- src/app/(public)/[locale]/layout.tsx
+++ src/app/(public)/[locale]/layout.tsx
@@ -181,10 +181,60 @@ export default async function LocaleLayout({
                         strategy="afterInteractive"
                     />
                 ) : null}
+
+                <Script
+                    id="yandex-context"
+                    src="https://yandex.ru/ads/system/context.js"
+                    strategy="afterInteractive"
+                />
+
+                <Script
+                    id="yandex-autoplacement"
+                    src="https://yandex.ru/ads/system/ap-loader.js"
+                    data-page-id="19707056"
+                    strategy="afterInteractive"
+                />
                 
-                {/* <!-- Yandex Autoplacement 19707056 --> */}
-                <script src="https://yandex.ru/ads/system/context.js" async></script>
-                <script data-page-id="19707056" src="https://yandex.ru/ads/system/ap-loader.js" async></script>
+                
+                <Script id="yandex-metrika" strategy="afterInteractive">
+                    {`
+                        (function(m,e,t,r,i,k,a){
+                            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                            m[i].l=1*new Date();
+                            for (var j = 0; j < document.scripts.length; j++) {
+                                if (document.scripts[j].src === r) {
+                                    return;
+                                }
+                            }
+                            k=e.createElement(t);
+                            a=e.getElementsByTagName(t)[0];
+                            k.async=1;
+                            k.src=r;
+                            a.parentNode.insertBefore(k,a);
+                        })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=111323165", "ym");
+
+                        ym(111323165, "init", {
+                            ssr: true,
+                            webvisor: true,
+                            clickmap: true,
+                            ecommerce: "dataLayer",
+                            referrer: document.referrer,
+                            url: location.href,
+                            accurateTrackBounce: true,
+                            trackLinks: true
+                        });
+                    `}
+                </Script>
+
+                <noscript>
+                    <div>
+                        <img
+                            src="https://mc.yandex.ru/watch/111323165"
+                            style={{ position: "absolute", left: "-9999px" }}
+                            alt=""
+                        />
+                    </div>
+                </noscript>
             </body>
         </html>
     );
